3c06933b5764b760e7f54e93c2c23a025b11e188
masendhy <massendhy@gmail.com>
2020-02-09 16:59:18 +0700

    fitur pofile done

--- PMS/routes/profile.js
+++ PMS/routes/profile.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const helpers = require("../helpers/util")
 
 module.exports = (pool) => {
 
@@ -7,9 +8,32 @@ module.exports = (pool) => {
 /* GET HOME PAGE */
 
 
-    router.get('/', function(req, res, next) {
-        res.render('profile/profile');
+    router.get('/', (req, res, next) => {
+      sql = `SELECT * FROM users WHERE email = '${req.session.user.email}'`;
+        pool.query(sql, (err, profile) => {
+        res.render('profile/profile',{
+          profile: profile.rows[0],
+          user: req.session.user,
+        });
       });
+    })
+
+    //update profile
+
+    /* post edit users. */
+    router.post('/', helpers.isLoggedIn, (req, res, next)=> {
+        const email = req.session.user.email;
+        let sql = `UPDATE users SET firstname = $1, lastname = $2,  password = $3, position = $4, isfulltime = $5  WHERE email = '${req.session.user.email}' `;
+        const data = [req.body.firstname, req.body.lastname, req.body.password, req.body.position, req.body.jobtype]
+
+        pool.query(sql, data, (err, data) => {
+            if (err) {
+                throw err
+            }
+            res.redirect('/projects');
+
+        });
+    });
 
       return router;
 }
